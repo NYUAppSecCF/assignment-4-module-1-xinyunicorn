5aa9ad6e20f6e105a8aa4b7681afc1d16b1d8140
Xin Yu Zhang <xz3979@nyu.edu>
2026-04-24 21:08:20 -0400

    Replaced implicit intents, enforced HTTPS, removed exported activities and intent filters, eliminated location/sensor tracking and unnecessary permissions, cleaned up privacy-violating code

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/SecondFragment.kt
@@ -66,10 +66,8 @@ class SecondFragment : Fragment() {
                         loggedInUser = response.body()
                         Log.d("Register Success", "Register success. Boo.")
                         Log.d("Register Success", "Token:" + loggedInUser?.token.toString())
-                        var intent = Intent(Intent.ACTION_VIEW)
-                        intent.type = "text/giftcards_browse"
-                        intent.data = Uri.parse("https://appsec.moyix.net/api/index")
-                        intent.putExtra("User", loggedInUser);
+                        val intent = Intent(requireContext(), ProductScrollingActivity::class.java)
+                        intent.putExtra("User", loggedInUser)
                         startActivity(intent)
                     }
                 }
